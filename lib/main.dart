99b822f19d36e3dc87ed1c5de299d205ad29c543
TambongSlade <tambongslade17@gmial.com>
2024-06-16 14:47:09 +0100

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:safespace/Screens/splashScreen.dart';
 
-void main() {
+
+
+void main() async {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
